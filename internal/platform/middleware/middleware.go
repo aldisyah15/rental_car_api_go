cd88b48c5f2a1118cd1c32bd8d04d2f051f46286
aldisyah15 <aldisyah687@gmail.com>
2026-07-16 20:57:02 +0700

    convert middleware from http to gin framework

--- internal/platform/middleware/middleware.go
+++ internal/platform/middleware/middleware.go
@@ -1,11 +1,12 @@
 package middleware
 
 import (
-	"context"
+	"log"
 	"net/http"
 	"strings"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 )
 
@@ -13,8 +14,8 @@ var secretKey = []byte("your-secret-key")
 
 func GenerateString(userName string) (string, error) {
 	claim := jwt.MapClaims{
-		"userId": userName,
-		"exp":    time.Now().Add(time.Hour * 24).Unix(),
+		"username": userName,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
@@ -23,31 +24,36 @@ func GenerateString(userName string) (string, error) {
 
 func veryfyToken(tokenString string) (string, error) {
 	claims := jwt.MapClaims{}
-	userId, ok := claims["userId"].(string)
+
 	token, err := jwt.ParseWithClaims(tokenString, &claims, func(token *jwt.Token) (any, error) {
 		return secretKey, nil
 	})
 
+	username, ok := claims["username"].(string)
+
 	if err == nil && token.Valid {
 		if ok == true {
-			return userId, nil
+			return username, nil
 		}
 	}
 
 	return "", err
 }
 
-func Middleware(next http.HandlerFunc) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		authorization := req.Header.Get("Authorization")
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authorization := c.GetHeader("Authorization")
 
-		userId, err := veryfyToken(strings.TrimPrefix(authorization, "Bearer "))
+		userName, err := veryfyToken(strings.TrimPrefix(authorization, "Bearer "))
 
 		if err != nil {
-			http.Error(w, "Forbidden", http.StatusForbidden)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Anda tidak memiliki akses"})
 			return
 		}
-		ctx := context.WithValue(req.Context(), "userId", userId)
-		next.ServeHTTP(w, req.WithContext(ctx))
-	})
+
+		log.Printf("log dari middleware: %v", userName)
+
+		c.Set("userName", userName)
+		c.Next()
+	}
 }
