4c254fafa4dd9cd0468adbb33e076f729bb31870
aldisyah15 <aldisyah687@gmail.com>
2026-07-18 01:41:15 +0700

    feat : add UpdateUser and DeleteUser

--- internal/user/useCase/useCaseUser.go
+++ internal/user/useCase/useCaseUser.go
@@ -1,6 +1,7 @@
 package useCase
 
 import (
+	"log"
 	"rental_car/internal/user/model"
 	"rental_car/internal/user/repository"
 )
@@ -9,9 +10,9 @@ type UserUseCase struct {
 	repository *repository.UserRepository
 }
 
-func NewUserUseCase(id *repository.UserRepository) *UserUseCase {
+func NewUserUseCase(username *repository.UserRepository) *UserUseCase {
 	return &UserUseCase{
-		repository: id,
+		repository: username,
 	}
 }
 
@@ -21,4 +22,39 @@ func (u UserUseCase) GetUser(username string) (*model.RespondUser, error) {
 	}
 	result, err := u.repository.GetUser(user.Username)
 	return result, err
+}
+
+func (u UserUseCase) UpdateUser(username string, req model.RequestUpdateUser) (*model.RespondUser, error) {
+	existingUser, err := u.repository.GetUser(username)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("log user useCase: %v", existingUser)
+
+	if req.Name != "" {
+		existingUser.Name = req.Name
+	}
+	if req.Username != "" {
+		existingUser.UserName = req.Username
+	}
+	if req.Phone != "" {
+		existingUser.Phone = req.Phone
+	}
+	if req.Email != "" {
+		existingUser.Email = req.Email
+	}
+
+	user := &model.RequestUpdateUser{
+		Name:     existingUser.Name,
+		Username: existingUser.UserName,
+		Phone:    existingUser.Phone,
+		Email:    existingUser.Email,
+	}
+	return u.repository.UpdateUser(username, user)
+}
+
+func (u UserUseCase) DeleteUser(username string) (id string, err error) {
+	id, err = u.repository.DeleteUser(username)
+	return id, err
 }
