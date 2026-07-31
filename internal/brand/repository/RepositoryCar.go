ae9b88ef503e7e4c6097440678017db58985586e
aldisyah15 <aldisyah687@gmail.com>
2026-07-31 12:40:27 +0700

    add : RepositoryCar.go

---
+++ internal/brand/repository/RepositoryCar.go
@@ -0,0 +1,45 @@
+package repository
+
+import (
+	"database/sql"
+	"rental_car/internal/brand/model"
+)
+
+type LogoRepository struct {
+	db *sql.DB
+}
+
+func NewRepositoryLogo(db *sql.DB) *LogoRepository {
+	return &LogoRepository{
+		db: db,
+	}
+}
+
+func (r LogoRepository) UploadLogo(urlLogo string, name string) error {
+	query := `INSERT INTO brand (brand, name) values (?,?)`
+	_, err := r.db.Exec(query, urlLogo, name)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r LogoRepository) GetAllLogo() (*[]model.ResponseLogo, error) {
+	query := `SELECT id, name, brand from brand`
+	rows, err := r.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var brans []model.ResponseLogo
+
+	for rows.Next() {
+		var result model.ResponseLogo
+		rows.Scan(&result.Id, &result.Name, &result.Logo)
+		brans = append(brans, result)
+	}
+
+	return &brans, err
+}
