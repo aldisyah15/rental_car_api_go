d20ef0e8ca64b6db92e2ebf7c40bc549341f230b
aldisyah15 <aldisyah687@gmail.com>
2026-07-31 12:38:26 +0700

    update : add HandlerLogo.go

---
+++ internal/brand/handler/HandlerLogo.go
@@ -0,0 +1,64 @@
+package handler
+
+import (
+	"log"
+	"net/http"
+	useCase2 "rental_car/internal/brand/useCase"
+	"rental_car/platform"
+
+	"github.com/gin-gonic/gin"
+)
+
+type LogoHandler struct {
+	useCase *useCase2.LogoUseCase
+}
+
+func NewCarHandlerLogo(carUseCase *useCase2.LogoUseCase) *LogoHandler {
+	return &LogoHandler{
+		useCase: carUseCase,
+	}
+}
+
+func (hc LogoHandler) UploadLogo(c *gin.Context) {
+	rule, _ := c.Get("rule")
+	if rule != "admin" {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "StatusUnauthorized",
+		})
+		return
+	}
+	name := c.PostForm("name")
+	err := c.ShouldBindJSON(&name)
+	fileName, _ := c.FormFile("brand")
+
+	url, err := platform.UploadFileAndGet(c.Request.Context(), fileName, "rental-car-app", "brand")
+	log.Printf("url : %v", url)
+
+	err = hc.useCase.UploadLogo(url, name)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "success",
+	})
+}
+
+func (hc LogoHandler) GetAllLogo(c *gin.Context) {
+	result, err := hc.useCase.GetAllLogo()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "success",
+		"data":    result,
+	})
+}
